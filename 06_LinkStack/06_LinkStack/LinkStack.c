0aea7bd842b76acc90881ceb47bedb88487e23c0
chan <chan@chandeMacBook-Pro.local>
2015-10-21 19:52:34 +0800

    实现

--- 06_LinkStack/06_LinkStack/LinkStack.c
+++ 06_LinkStack/06_LinkStack/LinkStack.c
@@ -7,39 +7,58 @@
 //
 
 #include "LinkStack.h"
+#include "LinkList.h"
+#include <stdlib.h>
 
+typedef struct _t_LinkStack_Node{
+    LinkListNode node;
+    void *val;
+}LinkStackNode;
 
 LinkStack *LinkStack_Create()
 {
-    return NULL;
+    return LinkList_Create();
 }
 
 void LinkStack_Destroy(LinkStack *stack)
 {
-    
+    //clear first
+    LinkStack_Clear(stack);
+    free(stack);
+    stack = NULL;
 }
 
 void LinkStack_Clear(LinkStack *stack)
 {
-    
+    while (LinkList_Length(stack)) {
+        LinkStack_Pop(stack);
+    }
 }
 
 int LinkStack_Push(LinkStack *stack, void *item)
 {
-    return 0;
+    //item 与linklist的结点结构不一样，需要包装一下
+    LinkStackNode *nod = malloc(sizeof(LinkStackNode));//alloc
+    nod->val = item;
+    return LinkList_Insert(stack, (LinkListNode *)nod, 0);
 }
 
 void *LinkStack_Pop(LinkStack *stack)
 {
-    return NULL;
+    LinkStackNode *nod = (LinkStackNode *)LinkList_Delete(stack, 0);
+    void *item = nod->val;
+    free(nod); //free
+    nod = NULL;
+    return item;
 }
 
 void *LinkStack_Top(LinkStack *stack)
 {
-    return NULL;
+    LinkStackNode *nod = (LinkStackNode *)LinkList_Get(stack, 0);
+    return nod->val;
 }
 
 int LinkStack_Size(LinkStack *stack)
 {
-    return 0;
+    return LinkList_Length(stack);
 }
--- 06_LinkStack/06_LinkStack/main.c
+++ 06_LinkStack/06_LinkStack/main.c
@@ -13,15 +13,15 @@ int main(int argc, const char * argv[]) {
     LinkStack *stack = NULL;
     int  a[5], i = 0;
     
-    for (i=0; i<5; i++)
+    for (i = 0; i < 5; i ++)
     {
         a[i] = i+1;
     }
     
     stack = LinkStack_Create();
     
-    //向栈中 添加 元素
-    for (i=0; i<5; i++)
+    //向栈中添加元素
+    for (i = 0; i < 5; i ++)
     {
         LinkStack_Push(stack, &a[i]); //抛砖: 栈的结点a[i]==>转换成(适配成) 链表结点 ===>插入到链表库
     }
@@ -30,15 +30,13 @@ int main(int argc, const char * argv[]) {
     printf("len:%d \n", LinkStack_Size(stack));
     printf("top:%d \n",  *( (int *)LinkStack_Top(stack) )  );
     
-    /*
-     //元素 出栈
+     //元素出栈
      while (LinkStack_Size(stack) > 0 )
      {
      int tmp = 0;
      tmp = *( (int *)LinkStack_Pop(stack) );
      printf("%d ", tmp);
      }
-     */
     
     LinkStack_Destroy(stack);
     return 0;
