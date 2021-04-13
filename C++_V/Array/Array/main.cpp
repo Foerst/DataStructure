c5329749617893e978e70351c5911729e01a38bd
edy.chen <edy.chen@gmail.com>
2021-04-13 14:09:16 +0800

    Update main.cpp

--- C++_V/Array/Array/main.cpp
+++ C++_V/Array/Array/main.cpp
@@ -29,10 +29,38 @@ public:
     }
 };
 
+class Solution1 {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        if (nums.size()==0) {
+            return vector<int>{-1, -1};
+        }
+
+        int l = 0;
+        int r = nums.size()-1;
+        while (l<=r) {
+            int mid = l + (r-l)/2;
+            if(nums[mid]==target){
+                int left = mid;
+                int right = mid;
+                while(left>0&&nums[left-1]==nums[left]) left--;
+                while(right+1<nums.size()&&nums[right+1]==nums[right]) right++;
+                return vector<int>{left, right};
+            } else if(nums[mid]<target){
+                l = mid+1;
+            } else {
+                r = mid-1;
+            }
+        }
+        return vector<int>{-1, -1};
+    }
+};
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    int a[] = {-2,-1};
-    vector<int> arr(a, a+sizeof(a)/sizeof(int));
-    int ret = Solution().maxSubArray(arr);
+//    int a[] = {-2,-1};
+//    vector<int> arr(a, a+sizeof(a)/sizeof(int));
+//    int ret = Solution().maxSubArray(arr);
+    vector<int> tmp{5,7,7,8,8,10};
+    vector<int> data = Solution1().searchRange(tmp, 8);
     return 0;
 }
